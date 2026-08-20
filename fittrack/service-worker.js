164b64b5514cbda8e7688773674329f43f82b75a
Kevin Arias <126743581+KevinAriass@users.noreply.github.com>
2026-08-19 23:04:15 -0600

    Update service-worker.js

--- fittrack/service-worker.js
+++ fittrack/service-worker.js
@@ -1,18 +1,51 @@
 
-// ============================================
-// FITTRACK PWA - SERVICE WORKER
-// ============================================
-
-const CACHE_NAME = 'fittrack-v1';
-const ASSETS_TO_CACHE = [
-    '/KevinAriasSolis.github.io/fittrack/',
-    '/KevinAriasSolis.github.io/fittrack/index.html',
-    '/KevinAriasSolis.github.io/fittrack/styles.css',
-    '/KevinAriasSolis.github.io/fittrack/app.js',
-    '/KevinAriasSolis.github.io/fittrack/manifest.json',
-    '/KevinAriasSolis.github.io/fittrack/icon-192.png',
-    '/KevinAriasSolis.github.io/fittrack/icon-512.png'
+var CACHE_NAME = 'fittrack-v2';
+var ASSETS = [
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
 self.addEventListener('install', function(event) {
-    event
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(ASSETS);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(names) {
+            return Promise.all(
+                names.filter(function(name) {
+                    return name !== CACHE_NAME;
+                }).map(function(name) {
+                    return caches.delete(name);
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).then(function(response) {
+            if (response && response.status === 200) {
+                var clone = response.clone();
+                caches.open(CACHE_NAME).then(function(cache) {
+                    cache.put(event.request, clone);
+                });
+            }
+            return response;
+        }).catch(function() {
+            return caches.match(event.request);
+        })
+    );
+});
+
